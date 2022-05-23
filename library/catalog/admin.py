72b1f6a1ac205241f971285583308285201f7951
Nurimov Amandos <amandosn@gmail.com>
2022-05-23 21:26:49 +0600

    added search_fileds, display_links in admin panel

--- library/catalog/admin.py
+++ library/catalog/admin.py
@@ -6,28 +6,34 @@ class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
 
 
-# admin.site.register(Book)
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
     inlines = [BooksInstanceInline]
+    list_display_links = ['title']
+    search_fields = ['title']
 
 
-# admin.site.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
-
     fields = [('first_name', 'last_name'), ('date_of_birth', 'date_of_death')]
+    list_display_links = ['first_name', 'last_name']
+    search_fields = ['last_name', 'first_name']
 
 
 admin.site.register(Author, AuthorAdmin)
-admin.site.register(Genre)
 
 
-# admin.site.register(BookInstance)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ['name']
+
+admin.site.register(Genre, GenreAdmin)
+
+
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_filter = ('status', 'due_back')
+    list_filter = ('book',)
     fieldsets = (
         (None, {
             'fields': ('book', 'imprint', 'id')
