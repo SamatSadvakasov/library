598242a4a232dbccba3e322b9b963c3e7773d43e
samat <sadvakasov.samat@gmail.com>
2022-05-25 19:28:14 +0600

    session was added

--- library/catalog/views.py
+++ library/catalog/views.py
@@ -4,6 +4,13 @@ from .models import Book, Author, BookInstance, Genre
 
 
 def index(request):
+    # EXAMPLE
+    # my_car = request.session.get('my_car', 'mini')
+    # request.session['my_car'] = 'mini'
+    # request.session.modified = True
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
     # Available books (status = 'a')
@@ -13,7 +20,8 @@ def index(request):
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
-        'num_authors': num_authors
+        'num_authors': num_authors,
+        'num_visits': num_visits
     }
     return render(request, 'catalog/index.html', context=context)
 
@@ -22,6 +30,7 @@ class BookListView(generic.ListView):
     model = Book
     paginate_by = 10
 
+
 class AuthorListView(generic.ListView):
     model = Author
     paginate_by = 10
@@ -29,6 +38,7 @@ class AuthorListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
 
 class AuthorDetailView(generic.DetailView):
     model = Author
