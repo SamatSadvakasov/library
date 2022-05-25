efa04b23796134100ae2abb84542a042c5902969
samat <sadvakasov.samat@gmail.com>
2022-05-25 19:38:32 +0600

    accounts was added in urls.py

--- library/library/urls.py
+++ library/library/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     path('catalog/', include('catalog.urls')),
 ]
 
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
