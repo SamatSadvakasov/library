261b573589298307a68980be8f89afa1cb68e7f3
AstanaRome <alikhan3991@gmail.com>
2022-06-11 16:02:30 +0600

    add authors

--- library/catalog/views.py
+++ library/catalog/views.py
@@ -32,3 +32,10 @@ class BookDetailView(generic.DetailView):
     model = Book
 
 
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 1
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
