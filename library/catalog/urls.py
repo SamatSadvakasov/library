875038af421baeb51a93d1a2793d5da2ade71cf3
AstanaRome <alikhan3991@gmail.com>
2022-06-13 01:06:15 +0600

    some changes

--- library/catalog/urls.py
+++ library/catalog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('borrowed_books/', views.LoanedBookListView.as_view(), name='all-borrowed'),
